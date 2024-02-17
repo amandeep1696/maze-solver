06683e0c227ded3a572d40e8ff10f4db600db54c
Amandeep Singh <am418470@dal.ca>
2024-02-17 13:06:43 -0400

    Add redraw method to window class

--- main_window.py
+++ main_window.py
@@ -3,11 +3,11 @@ from tkinter import Tk, BOTH, Canvas
 class Window:
     def __init__(self, width, height):
         self.root = Tk()
-
         self.root.title("Maze Solver")
-
         self.canvas = Canvas(self.root, width=width, height=height)
-
         self.canvas.pack(fill=BOTH, expand=True)
-        
         self.is_running = False
+    
+    def redraw(self):
+        self.root.update_idletasks()
+        self.root.update()
